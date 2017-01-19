a8dc0a505d450dd8d4e126af7919faad59c33327
Michael Watanabe <mikey.watanabe@gmail.com>
2017-01-19 15:30:11 -0500

    completed function return

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -2,11 +2,11 @@ func printAge(name: String) {
     print("\(name) is 29")
 }
 
-printAge("Adam")
+printAge(name:"Adam")
 
 
 func happyBirthday(age: Int) {
-    print("Happy birthday #\(age)!")
+    print("Happy \(age)th birthday!")
 }
 
 
@@ -20,18 +20,22 @@ func badGetAge(name: String) -> Int {
     print("\(name) is 29")
 }
 */
+let friends = "Ben"
+var friendAges = getAge(name: friends)
+happyBirthday(age: friendAges)
+
 
 
 let friend = "Emily"
-var friendAge = getAge(friend)
-happyBirthday(friendAge)
+var friendAge = getAge(name: friend)
+happyBirthday(age: friendAge)
 
 
 func birthdayGreeting(age: Int) -> String {
     return "Happy birthday #\(age)!"
 }
 
-var greeting = birthdayGreeting(30)
+var greeting = birthdayGreeting(age: 30)
 print(greeting)
 
 
@@ -42,4 +46,33 @@ func getAgeAndCongratulate(name: String) -> Int {
 }
 
 let friend2 = "Pete"
-var friend2Age = getAgeAndCongratulate(friend2)
+var friend2Age = getAgeAndCongratulate(name: friend2)
+
+func happyBirthdayBud(name: String) -> Int {
+    let age = 21
+    print("Happy \(age)st birthday, \(name)!")
+    return age
+}
+
+let friend3 = "Henry"
+var friend3Age = happyBirthdayBud(name: friend3)
+
+func lol(name: String) -> Int {
+    return 18
+    
+}
+
+var friendLol = lol(name: friend3)
+
+
+func somethingElse(name: String) -> Int {
+    let fun = 39
+    print("\(fun) is \(name)")
+    return fun
+}
+
+
+let dude = "buddy"
+
+var whatever = somethingElse(name: dude)
+
